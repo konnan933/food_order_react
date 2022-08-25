d44bbc7a4efbe77ca11c67759956d3a401f173e0
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-25 18:00:12 +0200

    Card, cart, meals hozzáadva

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,22 @@
 import classes from "./Cart.module.css";
 
-const Cart = (props) => {};
+const Cart = (props) => {
+  const cartItems = [{ id: 1, name: Sushi, amount: 2, price: 12.99 }].map(
+    (item) => <li>{item.name}</li>
+  );
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["classes--alt"]}>Close</button>
+        <button className={classes.button}></button>
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
