997f9c1f15e1dd56369199a45bcd44fff7d9c044
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-26 09:07:40 +0200

    useReducer használat

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,42 @@
+import { useReducer } from "react";
+
 import CartContex from "./cart-content";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat();
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState;
+};
+
 const CartProvide = (props) => {
-  const addItemHandler = (item) => {};
-  const removeItemHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemHandler = (item) => {
+    dispatchCartAction({ type: "REMOVE", item: item });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
